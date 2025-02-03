c15f54b1e79fc290571cc4a967bb93400901011b
Joseph Heinz <jbheinz14@gmail.com>
2025-02-03 12:04:24 -0600

    start on database setup (saving these for convenience)

---
+++ databaseCreation.sql
@@ -0,0 +1,41 @@
+-- Create a new UTF-8 `nbaoverunders` database.
+CREATE DATABASE nbaoverunders CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Switch to using the `nbaoverunders` database.
+USE nbaoverunders;
+
+-- Create an `entries` table.
+CREATE TABLE entries (
+	id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	playername VARCHAR(100) NOT NULL,
+	year INTEGER NOT NULL,
+	points DECIMAL(10, 2),
+	created DATETIME NOT NULL
+);
+-- Add an index on the created column.
+CREATE INDEX idx_entries_created ON entries(created);
+
+-- Add some dummy records
+INSERT INTO entries (playername, year, points, created) VALUES (
+	'Brendan Heinz',
+	2024,
+	20.50,
+	UTC_TIMESTAMP()
+);
+INSERT INTO entries (playername, year, points, created) VALUES (
+	'Thomas Bruch',
+	2024,
+	17.00,
+	UTC_TIMESTAMP()
+);
+INSERT INTO entries (playername, year, points, created) VALUES (
+	'Andy Heinz',
+	2024,
+	23.50,
+	UTC_TIMESTAMP()
+);
+
+-- create a web user - NO need to do this, already did as part of snippetbox
+CREATE USER 'web'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON entries.* TO 'web'@'localhost';
+-- Important: Make sure to swap 'pass' with a password of your own choosing.
+ALTER USER 'web'@'localhost' IDENTIFIED BY 'pass';
